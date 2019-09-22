ef4762b31249f48e7e1905cca771ea5af6a741da
Shaun <shaun@thenetninja.co.uk>
2019-09-22 15:54:26 +0100

    lesson-4

---
+++ brew_crew/lib/services/auth.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email and password
+
+
+  // register with email and password
+
+  // sign out
+
+}
