b2ec0d4ab16a84f787bf4c228a5ebf270ac57951
Shaun <shaun@thenetninja.co.uk>
2019-09-22 09:38:47 +0100

    lesson-3

--- brew_crew/ios/Flutter/flutter_export_environment.sh
+++ brew_crew/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\shaun\Documents\apps\flutter-firebase\brew_crew"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Shaun\Documents\Tutorial Code\YouTube\course-files\flutter-firebase\brew_crew"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
---
+++ brew_crew/lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate'),
+    );
+  }
+}
