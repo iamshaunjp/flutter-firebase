419e58057ac810e2bce1b24496397d47e750bca8
Shaun <shaun@thenetninja.co.uk>
2019-09-23 13:51:13 +0100

    lesson-7

--- brew_crew/lib/services/auth.dart
+++ brew_crew/lib/services/auth.dart
@@ -10,6 +10,13 @@ class AuthService {
     return user != null ? User(uid: user.uid) : null;
   }
 
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+      .map(_userFromFirebaseUser);
+  }
+
   // sign in anon
   Future signInAnon() async {
     try {
