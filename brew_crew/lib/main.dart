a517ac86d2ca6cd1980eda560deaaa3fbbdb6f0b
Shaun <shaun@thenetninja.co.uk>
2019-09-23 15:42:28 +0100

    lesson-9

--- brew_crew/lib/main.dart
+++ brew_crew/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
